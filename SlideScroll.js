3c73fd7ce4c60919ad8ee87dd96c1bfb02bb5b3d
PonomareVlad <sys.system@mail.ru>
2018-12-17 20:34:39 +0500

    Fix for current scroll position

--- SlideScroll.js
+++ SlideScroll.js
@@ -18,17 +18,17 @@ function scrollHandler(event) {
         const dimmThreshold = sectionOffset - window.innerHeight;
         // if (number === 2) console.log(document.body.scrollTop, sectionOffset, dimmThreshold);
         let dim = 0;
-        if (document.body.scrollTop < dimmThreshold) dim = 100;
-        else if (document.body.scrollTop > sectionOffset) dim = 0;
+        if (document.scrollingElement.scrollTop < dimmThreshold) dim = 100;
+        else if (document.scrollingElement.scrollTop > sectionOffset) dim = 0;
         else {
             // if (number === 2) console.log('section: 3', 'scrollTop: ' + document.body.scrollTop, 'sectionOffset: ' + sectionOffset,'sectionOffset-scrollTop:'+(sectionOffset-document.body.scrollTop),'opacityCoefficient: '+((sectionOffset-document.body.scrollTop)/(window.innerHeight/100)));
-            dim = ((sectionOffset - document.body.scrollTop) / (window.innerHeight / 100));
+            dim = ((sectionOffset - document.scrollingElement.scrollTop) / (window.innerHeight / 100));
             // if (number === 2) console.log(opacity, document.body.scrollTop, sectionOffset);
             // slideContent.style.opacity = opacity;
             // node.style.setProperty('--dimm-opacity', (100-opacity)/100);
             // console.log(opacity);
         }
-        // console.log(dim);
+        // if (number === 2) console.log(document.scrollingElement.scrollTop, sectionOffset, dimmThreshold, dim);
         if (node.dim !== dim) {
             node.style.setProperty('--dimm-opacity', dim / 100);
             node.dim = dim;
